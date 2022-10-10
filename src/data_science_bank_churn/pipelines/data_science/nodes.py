b25c1edce723e1a2990d9d497debf68722287aba
Luis Fernando Gonzalez Garay <lfgonzalez@applaudostudios.com>
2022-10-09 19:59:27 -0600

    Added visualization of model metrics

--- src/data_science_bank_churn/pipelines/data_science/nodes.py
+++ src/data_science_bank_churn/pipelines/data_science/nodes.py
@@ -10,12 +10,14 @@ from typing import List, Dict, Tuple, Any
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.model_selection import KFold, cross_val_predict, StratifiedKFold, StratifiedShuffleSplit
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, make_scorer
 from sklearn.metrics import f1_score, roc_auc_score
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def split_data(df: pd.DataFrame, target_variable: str, model_options_lg: Dict) -> Tuple:
     y = df[target_variable]
@@ -76,6 +78,20 @@ def train_model(x_train: pd.DataFrame, y_train: pd.Series, model_options_lg: Dic
             'svc_classifier__C': [2, 4, 6],
             'svc_classifier__kernel': ['rbf', 'sigmoid']
         }
+    elif model_options_lg['model'] == "RandomForest":
+        rf = RandomForestClassifier()
+
+        estimator = Pipeline([
+            # ("polynomial_features", PolynomialFeatures()),
+            ("scaler", ss),
+            ("RF_classifier", rf)])
+
+        params = {
+            # 'polynomial_features__degree': [1, 2,3],
+            'RF_classifier__n_estimators': [350, 400, 450],
+            'RF_classifier__max_depth': [None, 20],
+            'RF_classifier__warm_start': [True]
+        }
 
     grid = GridSearchCV(estimator, params, scoring=scoring, refit='f1', cv=skf, n_jobs=-1)
     grid.fit(x_train, y_train)
@@ -90,3 +106,8 @@ def evaluate_model(model: Any, x_test: pd.DataFrame, y_test: pd.Series):
     predictions = model.predict(x_test)
     print(classification_report(y_test, predictions))
     print(model.cv_results_['mean_test_f1'])
+    cr = classification_report(y_test, predictions, output_dict=True)
+    df_cr = pd.DataFrame(cr).iloc[:-1, :].T
+    sns.heatmap(df_cr, annot=True)
+
+    return plt
--- src/data_science_bank_churn/pipelines/data_science/pipeline.py
+++ src/data_science_bank_churn/pipelines/data_science/pipeline.py
@@ -8,7 +8,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import split_data, train_model, evaluate_model
 
 def create_pipeline(**kwargs) -> Pipeline:
-    pipeline_instance = pipeline(
+    pipeline_lr = pipeline(
         [
             node(
                 func=split_data,
@@ -19,30 +19,76 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=train_model,
                 inputs=["x_train", "y_train", "params:model_options_lg"],
-                outputs="model",
+                outputs="model_lg",
                 name="train_model_node",
             ),
             node(
                 func=evaluate_model,
-                inputs=["model","x_test","y_test"],
-                outputs=None,
+                inputs=["model_lg","x_test","y_test"],
+                outputs="classification_report_lr",
                 name="evaluate_model_node",
             ),
-        ]
-    )
-    ds_pipeline_1 = pipeline(
-        pipe=pipeline_instance,
+        ],
         inputs="encoded_data",
-        namespace="active_modelling_pipeline",
+        outputs="classification_report_lr",
+        namespace="active_modelling_pipeline"
     )
-    ds_pipeline_2 = pipeline(
-        pipe=pipeline_instance,
+
+    pipeline_svm = pipeline(
+        [
+            node(
+                func=split_data,
+                inputs=["encoded_data", "params:target", "params:model_options_svm"],
+                outputs=["x_train", "y_train", "x_test", "y_test"],
+                name="split_data_node",
+            ),
+            node(
+                func=train_model,
+                inputs=["x_train", "y_train", "params:model_options_svm"],
+                outputs="model_svm",
+                name="train_model_node",
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["model_svm", "x_test", "y_test"],
+                outputs="classification_report_svm",
+                name="evaluate_model_node",
+            ),
+        ],
         inputs="encoded_data",
-        namespace="candidate_modelling_pipeline",
-        parameters={"params:model_options_lg": "params:model_options_svm"},
+        outputs="classification_report_svm",
+        namespace="candidate_modelling_pipeline_svm"
     )
+
+    pipeline_rf = pipeline(
+        [
+            node(
+                func=split_data,
+                inputs=["encoded_data", "params:target", "params:model_options_rf"],
+                outputs=["x_train", "y_train", "x_test", "y_test"],
+                name="split_data_node",
+            ),
+            node(
+                func=train_model,
+                inputs=["x_train", "y_train", "params:model_options_rf"],
+                outputs="model_rf",
+                name="train_model_node",
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["model_rf", "x_test", "y_test"],
+                outputs="classification_report_rf",
+                name="evaluate_model_node",
+            ),
+        ],
+        inputs="encoded_data",
+        outputs="classification_report_rf",
+        namespace="candidate_modelling_pipeline_rf"
+    )
+
     return pipeline(
-        pipe=ds_pipeline_1 + ds_pipeline_2,
+        pipe=pipeline_lr + pipeline_svm + pipeline_rf,
         inputs="encoded_data",
+        outputs=["classification_report_lr","classification_report_svm","classification_report_rf"],
         namespace="data_science",
     )
