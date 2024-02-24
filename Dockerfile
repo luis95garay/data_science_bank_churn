b7cf1167e083d882f3c6fbac1673c0a1d7a33649
Luis Fernando González Garay <luis95.garay@gmail.com>
2024-02-24 15:51:00 -0600

    Add poetry

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,31 @@
 # Use the official Python 3.6 image as the base image
-FROM python:3.6-slim
+FROM python:3.9-slim
+
+# Install poetry
+RUN pip install poetry==1.4.2
+
+ENV POETRY_NO_INTERACTION=1 \
+    POETRY_VIRTUALENVS_IN_PROJECT=1 \
+    POETRY_VIRTUALENVS_CREATE=1 \
+    POETRY_CACHE_DIR=/tmp/poetry_cache
 
 # Set the working directory inside the container
 WORKDIR /app
 
-# Copy all files
-COPY . .
+# Copy poetry files
+COPY pyproject.toml poetry.lock ./
+RUN touch README.md
 
 # Install any dependencies specified in the requirements file
-RUN pip install --no-cache-dir -r src/requirements.txt
+RUN poetry install --without dev --no-root && rm -rf $POETRY_CACHE_DIR
+
+# Copy rest of the project
+COPY data_science_bank_churn ./data_science_bank_churn
+
+# Install any dependencies specified in the requirements file
+RUN poetry install --without dev
 
 # Expose the ports for each API (adjust the port numbers as needed)
 EXPOSE 8000
 
-ENTRYPOINT ["uvicorn", "--host", "0.0.0.0", "--port", "8000", "src.app:app"]
+ENTRYPOINT ["poetry", "run", "python", "-m", "uvicorn", "--host", "0.0.0.0", "--port", "8000", "data_science_bank_churn.src.app:app"]
---
+++ data_science_bank_churn/src/api/routes/prediction.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from fastapi import Request
+from fastapi.routing import APIRouter
+# from fastapi.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+
+from data_science_bank_churn.src.api.pipelines.prediction_pipeline import PredictPipeline, CustomData
+from data_science_bank_churn.src.api.schemas import EmployeeData
+
+
+router = APIRouter(tags=['prediction'])
+
+# templates_folder = Path(__file__).parent.parent.parent / "templates"
+# templates = Jinja2Templates(directory=str(templates_folder))
+
+
+# @router.get("/", response_class=HTMLResponse)
+@router.get("/")
+async def index(request: Request):
+    # return templates.TemplateResponse("index.html", {"request": request})
+    return {'result': 'exito'}
+
+
+@router.post("/predict")
+async def predict(request: Request, employeedata: EmployeeData):
+    data = CustomData(
+        employeedata.customer_age,
+        employeedata.gender,
+        employeedata.dependent_count,
+        employeedata.education_level,
+        employeedata.marital_status,
+        employeedata.income_category,
+        employeedata.card_category,
+        employeedata.months_on_book,
+        employeedata.total_relationship_count,
+        employeedata.months_inactive_12_mon,
+        employeedata.contacts_count_12_mon,
+        employeedata.credit_limit,
+        employeedata.total_revolving_Bal,
+        employeedata.avg_open_to_buy,
+        employeedata.total_amt_chng_q4_q1,
+        employeedata.total_trans_amt,
+        employeedata.total_trans_ct,
+        employeedata.total_ct_chng_q4_q1,
+        employeedata.avg_utilization_ratio
+    )
+    pred_df = data.get_data_as_data_frame()
+    predict_pipeline = PredictPipeline()
+    results = predict_pipeline.predict(pred_df)
+
+    # return templates.TemplateResponse("index.html", {"request": request, "results": results['result']})
+    return {'data': results}
