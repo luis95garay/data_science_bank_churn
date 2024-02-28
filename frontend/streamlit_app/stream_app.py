3c3ef71bd30b67a16f0bcc2a670da19b409778fa
Luis Fernando González Garay <luis95.garay@gmail.com>
2024-02-28 16:14:24 -0600

    test compose github action

--- frontend/streamlit_app/stream_app.py
+++ frontend/streamlit_app/stream_app.py
@@ -19,9 +19,11 @@ def predict_req(new_data: Dict):
     return details['data']
 
 
+st.set_page_config(layout="wide")
+
 st.title("Bank Churn prediction :bank:")
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4, gap="medium")
 with col1:
     customer_age = st.number_input('How old are you?', placeholder="Type a number", value=54, step=1)
     gender = st.selectbox(
@@ -34,24 +36,26 @@ with col1:
     marital_status = st.selectbox(
     'What is your marital status?',
     ('Married', 'Single', 'Unknown', 'Divorced'), index=0)
+
+with col2:
     income_category = st.selectbox(
     'What is your level of income?',
     ('Unknown', 'Less than $40K', '$40K - $60K', '$60K - $80K', '$80K - $120K', '$120K +'), index=3)
-
-with col2:
     card_category = st.selectbox(
     'What is your card category?',
     ('Blue', 'Gold', 'Silver', 'Platinum'), index=2)
     months_on_book = st.number_input('Months_on_book?', placeholder="Type a number", value=39, step=1)
     total_relationship_count = st.number_input('Total_Relationship_Count?', placeholder="Type a number", value=5, step=1)
     months_inactive_12_mon = st.number_input('Months_Inactive_12_mon?', placeholder="Type a number", value=1, step=1)
-    contacts_count_12_mon = st.number_input('Contacts_Count_12_mon?', placeholder="Type a number", value=3, step=1)
-    credit_limit = st.number_input('Credit_Limit?', placeholder="Type a number", value=12691, step=1)
     
 with col3:
+    contacts_count_12_mon = st.number_input('Contacts_Count_12_mon?', placeholder="Type a number", value=3, step=1)
+    credit_limit = st.number_input('Credit_Limit?', placeholder="Type a number", value=12691, step=1)
     total_revolving_bal = st.number_input('Total_Revolving_Bal?', placeholder="Type a number", value=777, step=1)
     avg_open_to_buy = st.number_input('Avg_Open_To_Buy?', placeholder="Type a number", value=11914, step=1)
     total_amt_chng_q4_q1 = st.number_input('Total_Amt_Chng_Q4_Q1?', placeholder="Type a number", value=1.335, step=0.001)
+
+with col4:
     total_trans_amt = st.number_input('Total_Trans_Amt?', placeholder="Type a number", value=1144, step=1)
     total_trans_ct = st.number_input('Total_Trans_Ct?', placeholder="Type a number", value=42, step=1)
     total_ct_chng_q4_q1 = st.number_input('Total_Ct_Chng_Q4_Q1?', placeholder="Type a number", value=1.625, step=0.001)
@@ -81,4 +85,7 @@ if submitted:
         "avg_utilization_ratio": avg_utilization_ratio
     }
     result = predict_req(new_data)
-    st.write((f"The prediction is: {result}"))
+    if result == 'No Churn':
+        st.title((f"The prediction is: {result} :smile:"))
+    else:
+        st.title((f"The prediction is: {result} :sweat:"))
